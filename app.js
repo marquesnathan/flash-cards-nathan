237776fd115644f0333065c83e75194f60b7ca08
marquesnathan <145395943+marquesnathan@users.noreply.github.com>
2024-11-21 14:20:46 -0300

    /21/11

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function criaCartao(categora, pergunta, resposta){
-let container = document.getElementById('container');
-let cartao = document.createElement('article');
-cartao.className = 'cartao';
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
 
-cartao.innerHTML = `
+    cartao.innerHTML = `
                 <div class="cartao__conteudo">
                     <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
@@ -17,13 +17,13 @@ cartao.innerHTML = `
 
     let respostaEstaVisivel = false
 
-    function viracartao(){
-         respostaEstaVisivel = ! respostaEstaVisivel;
-         cartao.classList.toggle('active', respostaEstaVisivel);
+    function viracartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
     }
-                container.appendChild(cartao);
+    container.appendChild(cartao);
 
-cartao.addEventlistener('click',viraCartao);
+    cartao.addEventlistener('click', viraCartao);
 
-container.appendChild(cartao);
+    container.appendChild(cartao);
 }
